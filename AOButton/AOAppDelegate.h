9149cdd27750d7a9c5e1169198b746c2d7b44dc4
@Oleg Alekseenko <@alekoleg@gmail.com>
2013-07-30 13:21:41 +0400

    Initial Commit

---
+++ AOButton/AOAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AOAppDelegate.h
+//  AOButton
+//
+//  Created by Alekseenko Oleg on 30.07.13.
+//  Copyright (c) 2013 alekoleg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AOViewController;
+
+@interface AOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) AOViewController *viewController;
+
+@end
---
+++ AOButton/AOViewController.h
@@ -0,0 +1,13 @@
+//
+//  AOViewController.h
+//  AOButton
+//
+//  Created by Alekseenko Oleg on 30.07.13.
+//  Copyright (c) 2013 alekoleg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AOViewController : UIViewController
+
+@end
